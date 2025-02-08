58a9f8cfc438c808318da29b6c8e09004b0fc397
Bellamkonda Aaditya <aadityasrikrishna1703@gmail.com>
2025-02-08 13:51:55 +0000

    D3.js integration

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve everything in the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
